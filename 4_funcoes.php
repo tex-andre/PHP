c240ecce4ce815165f6c2577d8b141230af851b4
tex-andre <aluno20577@ipt.pt>
2018-08-27 18:00:11 +0100

    Comments

--- 4_funcoes.php
+++ 4_funcoes.php
@@ -7,7 +7,8 @@
     </head>
     <body>
         <?php
-            include ("4.1_funcao_soma.php");
+            include ("4.1_funcao_soma.php"); //importa o conteúdo de um ficheiro PHP
+            //funções do ficheiro importado
             echo diz_ola("Andre");
             echo soma([1, 2, 3, 4, 5, 6, 7, 8, 9]);
         ?>
